49613ca42b424fea5cf514668a441957d5d14d70
0HuanyuLi0 <lihuanyu1024@outlook.com>
2021-06-18 11:46:06 +0930

    back to Normal

--- src/js/controller.js
+++ src/js/controller.js
@@ -114,7 +114,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  // console.log('Welcome!');
 };
 
 init();
